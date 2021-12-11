39253bab2a540d23fcb53b4ece358ac0264f3a2b
witherr <>
2021-12-11 22:37:05 +0000

    Fixed and optimized cog commands

--- cogs/admin.py
+++ cogs/admin.py
@@ -7,10 +7,7 @@ class Admin(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @property
-    def display_emoji(self) -> discord.PartialEmoji:
-        return discord.PartialEmoji(name='\N{HAMMER AND PICK}')
-
+    display_emoji = "⚒️"
       
     # cog
     @commands.is_owner()
@@ -65,38 +62,37 @@ class Admin(commands.Cog):
                brief = "Reloads a cog",
                help = "Reloads a cog with the name, dev only command.")
     async def _reload(self, ctx, cog):
-      #await self.bot.sync_commands()
-      try:
-        if cog == 'all':
-          try: # let me try
-            for cog_ext in list(self.bot.extensions):
-                self.bot.reload_extension(f'{cog_ext}')
-                await ctx.send(f':repeat: Reloaded cog `{cog_ext[11:]}`')
-                await asyncio.sleep(0.5)
-          except Exception as e:
+        #await self.bot.sync_commands()
+        try:
+            if cog == 'all':
+                try:
+                    cogs = []
+                    for cog_ext in list(self.bot.extensions):
+                        self.bot.reload_extension(cog_ext)
+                        cog_name = cog_ext[5:] if cog_ext.startswith("cogs.") else cog_ext
+                        cogs.append(f"\n:repeat: Reloaded cog `{cog_name}`")
+                    await ctx.send(", ".join(cogs))
+                except Exception as e:
+  	                raise e
+            else:
+                try:
+                    self.bot.reload_extension(f'cogs.{cog}')
+                except commands.ExtensionNotLoaded:
+                    await ctx.send(f":x: Cog `{cog}` not found.")
+                else:
+                    await ctx.send(f':repeat: Reloaded cog `{cog}`')
+        except Exception as e:
   	        raise e
-        else:
-          try:
-            self.bot.reload_extension(f'cogs.{cog}')
-          except commands.ExtensionNotLoaded:
-            await ctx.send(f":x: Cog `{cog}` not found.")
-          else:
-            await ctx.send(f':repeat: Reloaded cog `{cog}`')
-      except Exception as e:
-  	    raise e
 
     # cog all
     @cog.command(name = "all", aliases = ['a'], hidden = True, brief = "All cogs", help = "Lists all cogs, dev only command.")
     async def _all(self, ctx):
-        coglist = discord.Embed(title = "Cogs", description  = "List of all enabled cogs", colour = embed_colour)
+        extlist = discord.Embed(title = "Cogs", description  = "List of all enabled cogs", colour = embed_colour)
   		
-        for cog in bot.extensions:
-            if cog.startswith("cogs"):
-                cogn = cog.split(".")[1].capitalize()
-            else:
-                cogn = cog
-            coglist.add_field(name = cogn, value = str(cog), inline = False)
-        await ctx.send(embed = coglist)
+        for ext in bot.extensions:
+            extn = ext.split(".")[1].capitalize() if ext.startswith("cogs") else ext.capitalize()
+            extlist.add_field(name = extn, value = str(ext), inline = False)
+        await ctx.send(embed = extlist)
   		
 def setup(bot):
   bot.add_cog(Admin(bot))
