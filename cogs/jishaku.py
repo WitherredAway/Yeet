a292dc5ddbe638c1ef329bb96e934176e976bf1b
WitherredAway <>
2022-03-12 14:56:44 +0000

    - Fix inheritance of docstring - Redefine 'show' and 'hide' commands to work with custom hide feature.

--- cogs/jishaku.py
+++ cogs/jishaku.py
@@ -3,11 +3,14 @@ import discord
 from discord.ext import menus, commands
 from jishaku.cog import Jishaku
 from jishaku.codeblocks import codeblock_converter
+from jishaku.features.baseclass import Feature
 
 class Jishaku(Jishaku):
+    __doc__ = Jishaku.__doc__
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.bot = kwargs.pop('bot')
+        self.hidden = False
 
     @commands.command(
         name="git",
@@ -17,6 +20,35 @@ class Jishaku(Jishaku):
     async def _git(self, ctx: commands.Context, *, argument: codeblock_converter):
         jsk_git_command = self.bot.get_command('jishaku git')
         return await ctx.invoke(jsk_git_command, argument=argument)
+
+    @Feature.Command(parent="jsk", name="hide")
+    async def jsk_hide(self, ctx: commands.Context):
+        """
+        Hides Jishaku from the help command.
+        """
+
+
+        if self.hidden:
+            return await ctx.send("Jishaku is already hidden.")
+
+
+        self.hidden = True
+        await ctx.send("Jishaku is now hidden.")
+
+
+    @Feature.Command(parent="jsk", name="show")
+    async def jsk_show(self, ctx: commands.Context):
+        """
+        Shows Jishaku in the help command.
+        """
+
+
+        if not self.hidden:
+            return await ctx.send("Jishaku is already visible.")
+
+
+        self.hidden = False
+        await ctx.send("Jishaku is now visible.")
 
 
 def setup(bot):
