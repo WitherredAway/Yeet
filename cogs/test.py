c23b5373946071b4a7d1249439193e7334509f9b
WitherredAway <sdssouvic99@gmail.com>
2023-02-24 15:20:25 +0000

    Collapse large texts

--- cogs/test.py
+++ cogs/test.py
@@ -6,6 +6,8 @@ import discord
 from discord.ext import commands, tasks
 import gists
 
+from constants import NEW_LINE as NL
+
 
 SHEET_URL = os.getenv("AFD_SHEET_URL")
 AFD_GIST_ID = os.getenv("AFD_GIST_ID")
@@ -36,7 +38,7 @@ class Test(commands.Cog):
 
     display_emoji = "🧪"
 
-    def format_unreviewed(self, df, user, pkm_indexes):
+    def format_unreviewed(self, df: pd.DataFrame, user: discord.User, pkm_indexes: list):
         pkm_list = []
         for idx, pkm_idx in enumerate(pkm_indexes):
             pokename = df.loc[pkm_idx, "Pokemon"]
@@ -56,14 +58,15 @@ class Test(commands.Cog):
         - Comments: {comment}
             """
             text = f"""
-    1. `{pokename}` {comment_text if comment else ""}
-        - [Sheet location]({location})
-        - [Imgur]({link})
-            """
+1. `{pokename}` {comment_text if comment else ""}
+    - [Sheet location]({location})
+    - [Imgur]({link})"""
             pkm_list.append(text)
         format_list = "\n".join(pkm_list)
-        return_text = f"""- **{user}** [{len(pkm_list)}]
-{format_list}"""
+        return_text = f"""<details>
+<summary>{user} ({user.id}) - {len(pkm_list)}</summary>
+{format_list}
+</details>"""
         return return_text
 
     def validate_unclaimed(self):
@@ -164,6 +167,21 @@ class Test(commands.Cog):
 
         return ml_list, ml_list_mention, ml_amount
 
+    @commands.is_owner()
+    @commands.command()
+    async def forceupdate(self, ctx: commands.Context):
+        try:
+            del self.unc_amount
+            del self.unr_amount
+            del self.ml_amount
+        except AttributeError:
+            pass
+
+        await ctx.message.add_reaction("▶️")
+        self.update_pokemon.cancel()
+        self.update_pokemon.start()
+        await ctx.message.add_reaction("✅")
+
     # The task that updates the unclaimed pokemon gist
     @tasks.loop(minutes=5)
     async def update_pokemon(self):
@@ -187,33 +205,20 @@ class Test(commands.Cog):
         files = []
         if self.unc:
             updated.append(f"`Unclaimed pokemon` **({unc_amount})**")
-            unc_content = "## Count: %s\n## Pokemon: \n%s" % (
-                unc_amount,
-                "\n".join(unc_list) if unc_list else "None",
-            )
+            unc_content = f'# Unclaimed Pokemon\nCount: {unc_amount}\n## Pokemon: \n<details>\n<summary>Click to expand</summary>\n\n{NL.join(unc_list) if unc_list else "None"}\n\n</details>'
             files.append(gists.File(name=UNC_FILENAME, content=unc_content))
 
         if self.unr:
             updated.append(f"`Unreviewed pokemon` **({unr_amount})**")
-            unr_content = "## Count: %s\n## Users: \n%s" % (
-                unr_amount,
-                "\n".join(unr_list) if unr_list else "None",
-            )
+            unr_content = f'# Unreviewed Pokemon\nCount: {unr_amount}\n## Users: \n{NL.join(unr_list) if unr_list else "None"}'
             files.append(gists.File(name=UNR_FILENAME, content=unr_content))
 
         if self.ml:
-            updated.append(f"`Missing link pokemon` **({ml_amount})**")
-            ml_content = (
-                "## Count: %s\n## Users: \n%s\n\n\n## Copy & paste to ping:\n```\n%s\n```"
-                % (
-                    ml_amount,
-                    "\n".join(ml_list) if ml_list else "None",
-                    "\n".join(ml_list_mention) if ml_list else "None",
-                )
-            )
+            updated.append(f"`Incomplete pokemon` **({ml_amount})**")
+            ml_content = f'# Incomplete Pokemon\nCount: {ml_amount}\n## Users: \n<details>\n<summary>Click to expand</summary>\n\n{NL.join(ml_list) if ml_list else "None"}\n\n</details>\n\n## Copy & paste to ping:\n<details>\n<summary>Click to expand</summary>\n\n```\n{NL.join(ml_list_mention) if ml_list else "None"}\n```\n\n</details>'
             files.append(gists.File(name=ML_FILENAME, content=ml_content))
 
-        contents = f"""## Contents
+        contents = f"""# Contents
 - [{ML_FILENAME}](#file-incomplete-pokemon-md)
 - [{UNC_FILENAME}](#file-unclaimed-pokemon-md)
 - [{UNR_FILENAME}](#file-unreviewed-pokemon-md)"""
