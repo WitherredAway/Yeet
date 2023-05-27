c614377a411ef3b4ff323d2631270a4e0894a535
WitherredAway <sdssouvic99@gmail.com>
2023-05-27 21:32:58 +0530

    Remove gen7 learnset from moveinfo command

--- cogs/Poketwo/ext/poketwo_moves.py
+++ cogs/Poketwo/ext/poketwo_moves.py
@@ -16,16 +16,12 @@ if typing.TYPE_CHECKING:
 D = TypeVar("D", bound="Data")
 
 
-MOVES = "https://raw.githubusercontent.com/poketwo/pokedex/master/pokedex/data/csv/moves.csv"
-POKEMON_MOVES = "https://raw.githubusercontent.com/poketwo/pokedex/master/pokedex/data/csv/pokemon_moves.csv"
-POKETWO_MOVES = (
+MOVES = "https://raw.githubusercontent.com/poketwo/data/master/csv/moves.csv"
+POKEMON_MOVES = (
     "https://raw.githubusercontent.com/poketwo/data/master/csv/pokemon_moves.csv"
 )
-MOVE_NAMES = "https://raw.githubusercontent.com/poketwo/pokedex/master/pokedex/data/csv/move_names.csv"
-POKEMON_NAMES = "https://raw.githubusercontent.com/poketwo/pokedex/master/pokedex/data/csv/pokemon_species_names.csv"
-POKEMON_FORMS = "https://raw.githubusercontent.com/poketwo/pokedex/master/pokedex/data/csv/pokemon_forms.csv"
-POKEMON_FORM_NAMES = "https://raw.githubusercontent.com/poketwo/pokedex/master/pokedex/data/csv/pokemon_form_names.csv"
-POKETWO_NAMES = "https://raw.githubusercontent.com/poketwo/data/master/csv/pokemon.csv"
+MOVE_NAMES = "https://raw.githubusercontent.com/poketwo/data/master/csv/move_names.csv"
+POKEMON_NAMES = "https://raw.githubusercontent.com/poketwo/data/master/csv/pokemon.csv"
 
 ENGLISH_ID = 9
 
@@ -67,7 +63,7 @@ class Pokemon:
 class Move:
     id: int
     name: str
-    pokemon_objs: typing.Dict[int, typing.List[Pokemon]]
+    pokemon_objs: typing.List[Pokemon]
     power: int
     pp: int
     accuracy: int
@@ -79,17 +75,13 @@ class Move:
 
     @cached_property
     def pokemon(self) -> typing.Dict[int, typing.List[str]]:
-        pokemon = {}
-        for gen, pokemon_obj_list in self.pokemon_objs.items():
-            # This is a dict first so that there can only be one
-            # instead of set(), to keep the order
-            pokemon_dict = {
-                pkm.name: pkm.id
-                for pkm in sorted(pokemon_obj_list, key=lambda p: p.name)
-            }
-            pokemon[gen] = list(pokemon_dict.keys())
+        # THis is a dict first to prevent duplicate pokemon while staying ordered
+        pokemon = {
+            pkm.name: pkm.id
+            for pkm in sorted(self.pokemon_objs, key=lambda p: p.name)
+        }
 
-        return pokemon
+        return list(pokemon.keys())
 
 
 class Data:
@@ -131,70 +123,14 @@ class Data:
             "pokemon_move_method_id == 1 & version_group_id == 20", inplace=True
         )
 
-        # poketwo_moves.csv, poketwo's gen7 data
-        self.pkm_moves_data_7 = pd.read_csv(
-            POKETWO_MOVES,
-            index_col=0,
-        )
-        self.pkm_moves_data_7.query("pokemon_move_method_id == 1", inplace=True)
-
-        self.pkm_grouped = {
-            7: self.pkm_moves_data_7.groupby("move_id"),
-            8: self.pkm_moves_data.groupby("move_id"),
-        }
-
-        # pokemon_form_names.csv
-        self.pkm_form_names_data = pd.read_csv(
-            POKEMON_FORM_NAMES,
-            index_col=0,
-            usecols=["pokemon_form_id", "local_language_id", "pokemon_name"],
-        )
-        self.pkm_form_names_data.query(
-            "local_language_id == @ENGLISH_ID & pokemon_name == pokemon_name",
-            inplace=True,
-        )
-        self.pkm_form_names_data.columns = ["local_language_id", "name"]
-
-        # pokemon_forms.csv
-        self.pkm_forms_data = pd.read_csv(
-            POKEMON_FORMS,
-            index_col=0,
-            usecols=["id", "pokemon_id"],
-        )
-
-        # Here the 'pokemon_id' column in pkm_forms_data
-        # replaces the corresponding index column of pkm_form_names_data
-        self.pkm_form_names_data = self.pkm_form_names_data.reset_index()
-        self.pkm_form_names_data = self.pkm_form_names_data.assign(
-            pokemon_form_id=self.pkm_form_names_data.pokemon_form_id.map(
-                self.pkm_forms_data.pokemon_id
-            ).combine_first(self.pkm_form_names_data.pokemon_form_id)
-        ).set_index("pokemon_form_id")
+        self.pkm_grouped = self.pkm_moves_data.groupby("move_id")
 
         # pokemon_species_names.csv
-        self.pkm_names_data_8 = pd.read_csv(
+        self.pkm_names_data = pd.read_csv(
             POKEMON_NAMES,
-            index_col=0,
-            usecols=["pokemon_species_id", "local_language_id", "name"],
-        )
-        self.pkm_names_data_8.query("local_language_id == @ENGLISH_ID", inplace=True)
-
-        con = pd.concat([self.pkm_names_data_8, self.pkm_form_names_data])
-        con = con[
-            ~con.index.duplicated(keep="first")
-        ]  # Keep the first instance of duplicate indices
-
-        self.pkm_names_data_8 = con
-
-        # pokemon.csv, poketwo's gen7 data
-        self.pkm_names_data_7 = pd.read_csv(
-            POKETWO_NAMES,
             index_col=0,
             usecols=["id", "name.en"],
         )
-        self.pkm_names_data_7.columns = ["name"]
-
-        self.pkm_names_data = {7: self.pkm_names_data_7, 8: self.pkm_names_data_8}
 
     @cached_property
     def moves(self) -> typing.Dict[int, Move]:
@@ -257,29 +193,27 @@ class Data:
         return DAMAGE_CLASSES
 
     def get_move_pokemon(self, move_id) -> typing.List[str]:
-        pkm_grouped = self.pkm_grouped
+        grouped = self.pkm_grouped
 
-        pokemon = {7: [], 8: []}
-        for gen, grouped in pkm_grouped.items():
-            pkm_names = self.pkm_names_data[gen]
+        pokemon = []
+        pkm_names = self.pkm_names_data
 
-            move_group = (
-                grouped.get_group(move_id) if move_id in grouped.groups else None
-            )
-            if move_group is not None:
-                for pkm_id, row in move_group.iterrows():
-                    if gen == 7:
-                        if (self.pk.loc[self.pk["id"] == pkm_id, "enabled"].values[0] > 0) is False:
-                            continue
-                    pkm_name = pkm_names.loc[pkm_id, "name"]
+        move_group = (
+            grouped.get_group(move_id) if move_id in grouped.groups else None
+        )
+        if move_group is not None:
+            for pkm_id, row in move_group.iterrows():
+                if (self.pk.loc[self.pk["id"] == pkm_id, "enabled"].values[0] > 0) is False:
+                    continue
+                pkm_name = pkm_names.loc[pkm_id, "name.en"]
 
-                    pokemon[gen].append(
-                        Pokemon(
-                            id=int(pkm_id),
-                            name=str(pkm_name),
-                            level=int(row.loc["level"]),
-                        )
+                pokemon.append(
+                    Pokemon(
+                        id=int(pkm_id),
+                        name=str(pkm_name),
+                        level=int(row.loc["level"]),
                     )
+                )
 
         return pokemon
 
@@ -299,37 +233,31 @@ class PoketwoMoves(commands.Cog):
         return self.bot.p2_data
 
     async def format_movesets_message(self, move: Move):
-        gen_7_pokemon = move.pokemon[7]
-        len_gen_7 = len(gen_7_pokemon)
-        gen_7_list = ", ".join(gen_7_pokemon) if len_gen_7 > 0 else "None"
+        pokemon = move.pokemon
+        joined = ", ".join(pokemon) if len(pokemon) > 0 else "None"
+        joined = CODE_BLOCK_FMT % joined
 
-        gen_8_pokemon = move.pokemon[8]
-        len_gen_8 = len(gen_8_pokemon)
-        gen_8_list = ", ".join(gen_8_pokemon) if len_gen_8 > 0 else "None"
+        format = (
+            f"__**{move.name}**__\n"
+            f"**Type:** {move.type}\n"
+            f"**Class:** {move.damage_class}\n\n"
+            f"**Leveling learnset, in *Gen 8 (Galar)*** [`{len(pokemon)}`]\n"
+            f"%s"
+        )
 
-        if (len(gen_7_list) + len(gen_8_list)) > 1819:  # Character limit
-            gen_7_pokemon = sorted(
-                [[pkm.name, pkm.level] for pkm in move.pokemon_objs[7]],
-                key=lambda p: p[1],
-            )
-            gen_7_df = pd.DataFrame(
-                gen_7_pokemon,
-                columns=["Pokemon (Poketwo - Gen 7)", "Required level"],
-            )
+        final = format % joined
+        if len(final) > 2000:  # Character limit
 
-            gen_8_pokemon = sorted(
-                [[pkm.name, pkm.level] for pkm in move.pokemon_objs[8]],
+            pokemon = sorted(
+                [[pkm.name, pkm.level] for pkm in move.pokemon_objs],
                 key=lambda p: p[1],
             )
             gen_8_df = pd.DataFrame(
-                gen_8_pokemon,
+                pokemon,
                 columns=["Pokemon (Gen 8)", "Required level"],
             )
 
             files = [
-                gists.File(
-                    name="gen_7_table.csv", content=gen_7_df.to_csv(index=False)
-                ),
                 gists.File(
                     name="gen_8_table.csv", content=gen_8_df.to_csv(index=False)
                 ),
@@ -339,26 +267,9 @@ class PoketwoMoves(commands.Cog):
                 files=files, description=description, public=False
             )
 
-            gen_7_list = (
-                f"<{gist.url}#file-gen_7_table-csv>"  # Header of the gen 7 file
-            )
-            gen_8_list = (
-                f"<{gist.url}#file-gen_8_table-csv>"  # Header of the gen 8 file
-            )
-        else:
-            gen_7_list = CODE_BLOCK_FMT % gen_7_list
-            gen_8_list = CODE_BLOCK_FMT % gen_8_list
+            final = format % f"<{gist.url}#file-gen_8_table-csv>"  # Header of the gen 8 file
 
-        format = (
-            f"__**{move.name}**__\n"
-            f"**Type:** {move.type}\n"
-            f"**Class:** {move.damage_class}\n\n"
-            f"**Leveling learnset, in *Poketwo - Gen 7 (Alola)*** [`{len_gen_7}`]\n"
-            f"{gen_7_list}\n"
-            f"**Leveling learnset, in *Gen 8 (Galar)*** [`{len_gen_8}`]\n"
-            f"{gen_8_list}"
-        )
-        return format
+        return final
 
     @commands.group(
         name="moveinfo",
